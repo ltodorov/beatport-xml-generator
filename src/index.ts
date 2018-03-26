56ef13b6d65a793809329812a802f23ad8a614b2
Lyubomir Todorov <lyubomir.todorov@gmail.com>
2018-03-26 21:28:12 +0300

    Add NSR122

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as fs from "fs";
 import * as path from "path";
 import { Parser } from "./parser";
 import { templateRelease } from "./templates";
-import { release } from "./data/nsr120";
+import { release } from "./data/nsr122";
 
 const folder = path.resolve(__dirname, "../catalogue");
 const parser = new Parser(release);
