161e2a26ad018479ba1f6923f0375cf26d758dc3
Lyubomir Todorov <lyubomir.todorov@gmail.com>
2018-03-26 21:31:34 +0300

    Change NSR121

--- src/data/nsr121.ts
+++ src/data/nsr121.ts
@@ -2,7 +2,7 @@ import { Release, Track, GENRES } from "../models";
 
 export const release = new Release({
   id: 121,
-  originalReleaseDate: "2018-05-01",
+  originalReleaseDate: "2018-04-15",
   UPC_EAN: 192641057700,
   title: "Deep Dive",
   tracks: [
