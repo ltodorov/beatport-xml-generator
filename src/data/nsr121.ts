01da693b0b626043f658d8095eff7384cf4255c8
Lyubomir Todorov <lyubomir.todorov@gmail.com>
2018-03-26 21:09:48 +0300

    Add NSR121

---
+++ src/data/nsr121.ts
@@ -0,0 +1,19 @@
+import { Release, Track, GENRES } from "../models";
+
+export const release = new Release({
+  id: 121,
+  originalReleaseDate: "2018-05-01",
+  UPC_EAN: 192641057700,
+  title: "Deep Dive",
+  tracks: [
+    new Track({
+      ISRC: "USA2P1820002",
+      title: "Deep Dive",
+      mixVersion: "Original Mix",
+      artists: ["DJ Tarkan"],
+      remixers: [],
+      songwriters: ["Tarkan Ors"],
+      genre: GENRES.DeepHouse
+    })
+  ]
+});
