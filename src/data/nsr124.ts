f4aa031f0f6c788b22a6da714c1e2cff15db3af3
Lyubomir Todorov <lyubomir.todorov@gmail.com>
2018-05-14 08:55:52 +0300

    Add NSR124

--- src/data/nsr124.ts
+++ src/data/nsr124.ts
@@ -3,16 +3,16 @@ import { Release, Track, GENRES } from "../models";
 export const release = new Release({
   id: 124,
   originalReleaseDate: "2018-06-01",
-  UPC_EAN: 0,
-  title: "Deep Down",
+  UPC_EAN: 192641092879,
+  title: "Get Better Gurkan Asik Remix",
   tracks: [
     new Track({
-      ISRC: "",
-      title: "Deep Down feat. Zara",
-      mixVersion: "RoelBeat Remix",
+      ISRC: "USA2P1830592",
+      title: "Get Better feat. Yalena",
+      mixVersion: "Gurkan Asik Remix",
       artists: ["DJ Tarkan"],
-      remixers: ["RoelBeat"],
-      songwriters: ["Maxim Poduschenko"],
+      remixers: ["Gurkan Asik"],
+      songwriters: ["Gurkan Asik"],
       genre: GENRES.DeepHouse
     })
   ]
